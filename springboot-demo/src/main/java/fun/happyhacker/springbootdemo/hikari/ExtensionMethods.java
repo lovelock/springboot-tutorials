d6bfede7a938095fb65d650e9e568fe4859b75f5
qingchun3 <qingchun3@staff.weibo.com>
2021-01-22 17:13:15 +0800

    mybatis开始

--- springboot-demo/src/main/java/fun/happyhacker/springbootdemo/hikari/ExtensionMethods.java
+++
@@ -1,9 +0,0 @@
-package fun.happyhacker.springbootdemo.hikari;
-
-import com.google.gson.GsonBuilder;
-
-public class ExtensionMethods {
-    public static String toJson(Object object) {
-        return new GsonBuilder().serializeNulls().create().toJson(object);
-    }
-}
--- springboot-demo/src/main/java/fun/happyhacker/springbootdemo/jpa/embeddable/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package fun.happyhacker.springbootdemo.jpa.embeddable.repository;
-
-import fun.happyhacker.springbootdemo.jpa.embeddable.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-}
