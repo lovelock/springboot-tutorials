a1782b029c6fde37b36452a982edad830d609bc6
qingchun3 <qingchun3@staff.weibo.com>
2021-01-25 16:36:06 +0800

    删除自动生成的文件

--- springboot-demo/src/main/java/fun/happyhacker/springbootdemo/mybatis/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package fun.happyhacker.springbootdemo.mybatis.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import fun.happyhacker.springbootdemo.mybatis.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-//@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
